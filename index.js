2e16394f4eddb66468a917fc92d97b608b432c53
Big mike <mikendirangu26@gmail.com>
2025-06-14 21:07:03 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// Create two numbers that multiply to 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Create two numbers where the remainder is 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Find the highest number in a set where the highest is 20
+const max = Math.max(1, 3, 5, 10, 20);
